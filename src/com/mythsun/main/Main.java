62c0151c4c5d1605a69307639f53a74f7cdcf38b
3154365988 <3154365988@qq.com>
2021-01-06 22:50:26 +0800

    增加RegexUtil

---
+++ src/com/mythsun/main/Main.java
@@ -0,0 +1,20 @@
+package com.mythsun.main;
+
+import com.mythsun.tools.RegexUtil;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        String filePath = "F:\\aaa.txt";
+        String regex = "fg|rtt";
+        try {
+            String s = RegexUtil.matchFileContent(filePath,regex);
+            System.out.println(s);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ src/com/mythsun/tools/RegexUtil.java
@@ -0,0 +1,35 @@
+package com.mythsun.tools;
+
+import java.io.*;
+import java.util.regex.Pattern;
+
+public class RegexUtil {
+
+    /**
+     * 对文件内容正则匹配
+     * @param filePath 文件路径
+     * @param regex 正则表达式
+     * @return 所有符合的行
+     * @throws Exception
+     */
+    public static String matchFileContent(String filePath, String regex) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        File file = new File(filePath);
+        if (!file.exists()){
+            throw new FileNotFoundException();
+        }
+        InputStreamReader reader = new InputStreamReader(new FileInputStream(file));
+        BufferedReader br = new BufferedReader(reader);
+        String line;
+        while ((line = br.readLine()) != null) {
+            if (Pattern.compile(regex).matcher(line).find()) {
+                sb.append(line);
+                sb.append("\n");
+            }
+        }
+        reader.close();
+        br.close();
+        return sb.toString().trim();
+    }
+
+}
